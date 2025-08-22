2a849b948bc50afc24afd75d2aca51d35b1ee8fa
Filippo Dembech <filippo.dembech@gmail.com>
2025-08-22 17:28:17 +0200

    refactor: extract deleteCommand() from cli.tsx

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 import meow from 'meow';
 import chalk from 'chalk';
-import {
-	deleteProject,
-	getProjects,
-	isExistingProject,
-} from './db.js';
-import readline from 'readline';
+import {getProjects} from './db.js';
 import {helpMessage} from './help.js';
-import { getdfCommand, listCommand, loadCommand, purgeCommand, saveCommand, setdfCommand } from './commands.js';
+import {
+	deleteCommand,
+	getdfCommand,
+	listCommand,
+	loadCommand,
+	purgeCommand,
+	saveCommand,
+	setdfCommand,
+} from './commands.js';
 
 const projects = getProjects();
 
@@ -29,16 +32,9 @@ const cli = meow(helpMessage, {
 	},
 });
 
-function hasCommand(): boolean {
-	return cli.input.length !== 0;
-}
+const hasCommand = cli.input.length !== 0;
 
-function hasProjectArgs(): boolean {
-	const [_, ...args] = cli.input;
-	return args.length !== 0;
-}
-
-if (!hasCommand()) {
+if (!hasCommand) {
 	cli.showHelp();
 } else {
 	const [command, ...args] = cli.input;
@@ -54,64 +50,9 @@ if (!hasCommand()) {
 	} else if (command === 'load') {
 		loadCommand(args, cli.flags.shell);
 	} else if (command === 'purge') {
-		purgeCommand()
-		// if (projects.length === 0) {
-		// 	console.log('No Project is present.\n');
-		// 	console.log("Use 'projector save' to save new projects.");
-		// 	console.log(
-		// 		"You can also use '.psup' files with the '--source' option to save multiple projects faster.",
-		// 	);
-		// 	console.log("Type 'projector --help, -h' for help.");
-		// } else {
-		// 	const answer: string = await new Promise(resolve => {
-		// 		const prompt = readline.createInterface({
-		// 			input: process.stdin,
-		// 			output: process.stdout,
-		// 		});
-		// 		prompt.question(`Are you sure you wanna purge the db? (y/n) `, ans => {
-		// 			prompt.close();
-		// 			resolve(ans);
-		// 		});
-		// 	});
-
-		// 	if (answer.toLowerCase() === 'y') {
-		// 		for (let project of projects) {
-		// 			if (!fs.existsSync(project.folder)) {
-		// 				deleteProject(project);
-		// 			}
-		// 		}
-		// 	}
-		// }
+		purgeCommand();
 	} else if (command === 'delete') {
-		if (!hasProjectArgs()) console.log('No project has been passed');
-		else {
-			for (let projectName of args) {
-				// check if the passed project exist
-				if (!isExistingProject(projectName)) {
-					console.log(`Project '${chalk.blue.bold(projectName)}' not found!`);
-				} else {
-					const project = projects.find(p => p.name === projectName)!; // using '!' because project must exist because of 'isExistingProject()' check before
-
-					const answer: string = await new Promise(resolve => {
-						const prompt = readline.createInterface({
-							input: process.stdin,
-							output: process.stdout,
-						});
-						prompt.question(
-							`Do you want to delete '${chalk.blue.bold(projectName)}'? (y/n) `,
-							ans => {
-								prompt.close();
-								resolve(ans);
-							},
-						);
-					});
-
-					if (answer.toLowerCase() === 'y') {
-						deleteProject(project);
-					}
-				}
-			}
-		}
+		deleteCommand(args);
 	} else {
 		console.log(chalk.yellow(`command '${command}' does not exist.`));
 		console.log(cli.showHelp());
--- source/commands.tsx
+++ source/commands.tsx
@@ -7,12 +7,21 @@ import {printWarning} from './utils.js';
 import {parseProjectSetupFile} from './parser.js';
 import {isValidProject} from './validation.js';
 import {error} from './errors/errors.js';
-import {deleteProject, existProjectFolder, getDefaultFolder, getProjectFolder, getProjects, isExistingProject, saveProject, setDefaultFolder} from './db.js';
+import {
+	deleteProject,
+	existProjectFolder,
+	getDefaultFolder,
+	getProjectFolder,
+	getProjects,
+	isExistingProject,
+	saveProject,
+	setDefaultFolder,
+} from './db.js';
 import {render} from 'ink';
 import {Project} from './types.js';
 import Projects from './Projects.js';
 import path from 'path';
-import { execa, ExecaError } from 'execa';
+import {execa, ExecaError} from 'execa';
 import readline from 'readline';
 
 export async function saveCommand(projectSetupFile?: string) {
@@ -117,7 +126,9 @@ export async function loadCommand(args: string[], shellFlag?: string) {
 				);
 			} else {
 				console.log(`loading project '${chalk.blue.bold(projectName)}'...`);
-				const project = getProjects().find(project => project.name === projectName)!; // using '!' because project must exist because of 'isExistingProject()' check before
+				const project = getProjects().find(
+					project => project.name === projectName,
+				)!; // using '!' because project must exist because of 'isExistingProject()' check before
 				try {
 					for (let command of project.setupCommands) {
 						const {stdout} = shellFlag
@@ -139,32 +150,65 @@ export async function loadCommand(args: string[], shellFlag?: string) {
 }
 
 export async function purgeCommand() {
-    const projects = getProjects();
-		if (projects.length === 0) {
-			console.log('No Project is present.\n');
-			console.log("Use 'projector save' to save new projects.");
-			console.log(
-				"You can also use '.psup' files with the '--source' option to save multiple projects faster.",
-			);
-			console.log("Type 'projector --help, -h' for help.");
-		} else {
-			const answer: string = await new Promise(resolve => {
-				const prompt = readline.createInterface({
-					input: process.stdin,
-					output: process.stdout,
-				});
-				prompt.question(`Are you sure you wanna purge the db? (y/n) `, ans => {
-					prompt.close();
-					resolve(ans);
-				});
+	const projects = getProjects();
+	if (projects.length === 0) {
+		console.log('No Project is present.\n');
+		console.log("Use 'projector save' to save new projects.");
+		console.log(
+			"You can also use '.psup' files with the '--source' option to save multiple projects faster.",
+		);
+		console.log("Type 'projector --help, -h' for help.");
+	} else {
+		const answer: string = await new Promise(resolve => {
+			const prompt = readline.createInterface({
+				input: process.stdin,
+				output: process.stdout,
 			});
+			prompt.question(`Are you sure you wanna purge the db? (y/n) `, ans => {
+				prompt.close();
+				resolve(ans);
+			});
+		});
 
-			if (answer.toLowerCase() === 'y') {
-				for (let project of projects) {
-					if (!fs.existsSync(project.folder)) {
-						deleteProject(project);
-					}
+		if (answer.toLowerCase() === 'y') {
+			for (let project of projects) {
+				if (!fs.existsSync(project.folder)) {
+					deleteProject(project);
 				}
 			}
 		}
+	}
+}
+
+export async function deleteCommand(args: string[]) {
+	const projects = getProjects();
+	if (args.length === 0) console.log('No project has been passed');
+	else {
+		for (let projectName of args) {
+			// check if the passed project exist
+			if (!isExistingProject(projectName)) {
+				console.log(`Project '${chalk.blue.bold(projectName)}' not found!`);
+			} else {
+				const project = projects.find(p => p.name === projectName)!; // using '!' because project must exist because of 'isExistingProject()' check before
+
+				const answer: string = await new Promise(resolve => {
+					const prompt = readline.createInterface({
+						input: process.stdin,
+						output: process.stdout,
+					});
+					prompt.question(
+						`Do you want to delete '${chalk.blue.bold(projectName)}'? (y/n) `,
+						ans => {
+							prompt.close();
+							resolve(ans);
+						},
+					);
+				});
+
+				if (answer.toLowerCase() === 'y') {
+					deleteProject(project);
+				}
+			}
+		}
+	}
 }
